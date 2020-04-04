ebe829acfc667bde1a66b5264f3595bf60a90ee4
Masao Kitamura <masaok@gmail.com>
2020-04-04 03:16:15 -0700

    Made graph full width; slanted labels; testing other datasets

--- src/pages/DailyUS.tsx
+++ src/pages/DailyUS.tsx
@@ -6,7 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 // https://recharts.org/en-US/examples/SimpleLineChart
 import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
 } from 'recharts'
 
 // https://material-ui.com/components/tables/#dense-table
@@ -270,48 +270,35 @@ const DailyUS = (props: DailyUSProps) => {
       })
     )
 
-  const data = [
-    {
-      name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-    },
-    {
-      name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-    },
-    {
-      name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-    },
-    {
-      name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-    },
-    {
-      name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-    },
-    {
-      name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-    },
-    {
-      name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-    },
-  ];
-
   return (
     <>
-      <LineChart
-        width={800}
-        height={300}
-        data={dailyDataList.slice().reverse()}
-        margin={{
-          top: 5, right: 30, left: 20, bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="positive" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="negative" stroke="#82ca9d" />
-      </LineChart>
+      <ResponsiveContainer width='100%' aspect={16.0 / 5.0}>
+        <LineChart
+          data={dailyDataList.slice().reverse()}
+          margin={{
+            top: 20, right: 50, left: 50, bottom: 20
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          {/* Slanted Labels: https://github.com/recharts/recharts/issues/466 */}
+          {/* TODO: better way: http://recharts.org/en-US/examples/CustomizedLabelLineChart */}
+          <XAxis dataKey="date" height={80} angle={-45} textAnchor="end" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          {/* <Line type="monotone" dataKey="positive" stroke="#8884d8" /> */}
+          {/* <Line type="monotone" dataKey="negative" stroke="#82ca9d" /> */}
+
+          <Line dataKey="negative" stroke="green" />
+          {/* <Line dataKey="positive" stroke="red" /> */}
+          {/* <Line dataKey="hospitalized" stroke="purple" /> */}
+          <Line dataKey="death" stroke="black" />
+
+          <Line dataKey="hospitalizedIncrease" stroke="purple" />
+          <Line dataKey="deathIncrease" stroke="purple" />
+
+        </LineChart>
+      </ResponsiveContainer>
 
       <TableContainer component={Paper}>
         <Table className={classes.table} size="small" aria-label="main table">
